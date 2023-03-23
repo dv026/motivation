e2cbe8df0cf16a8aedd859498759859deca0c9c4
Dmitrii Vasilev <dimavas026@yandex.ru>
2023-03-23 09:04:29 +0300

    [2-23]: bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig, type PluginOption } from 'vite'
+
 
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [svgr(), react(), visualizer({
+    template: "sunburst", // or sunburst
+    open: true,
+    gzipSize: true,
+    brotliSize: true,
+    filename: "analice.html"
+  }) as PluginOption],
   resolve: {
     // it shows to vite how to build
     alias: {
